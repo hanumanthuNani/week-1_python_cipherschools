5334c6fdd6a853071576806fa75cb8361b06cc55
Hanumanthu Nani <119041453+hanumanthuNani@users.noreply.github.com>
2022-12-02 00:20:26 +0530

    Create class7-Looping Statements(while, for)-CipherSchools.py

---
+++ class7-Looping Statements(while, for)-CipherSchools.py
@@ -0,0 +1,12 @@
+#while Loop
+
+a=1
+while a<10:
+    print(a)
+    a+=1
+#When do we use while and for?
+'''
+If we don,t know how many iteration will it take to end this loop
+then we use while loop
+E.g: Temple Run
+'''
